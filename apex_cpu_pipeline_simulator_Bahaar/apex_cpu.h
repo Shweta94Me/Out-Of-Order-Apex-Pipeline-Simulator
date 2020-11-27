548e3cfd1b074369c9d750a362e6edd32fab89ee
bahaarsadhnani <bahaarsadhnani@gmail.com>
2020-11-26 23:39:04 -0500

    Added ROB Structure

---
+++ apex_cpu_pipeline_simulator_Bahaar/apex_cpu.h
@@ -0,0 +1,84 @@
+/*
+ * apex_cpu.h
+ * Contains APEX cpu pipeline declarations
+ *
+ * Author:
+ * Copyright (c) 2020, Gaurav Kothari (gkothar1@binghamton.edu)
+ * State University of New York at Binghamton
+ */
+#ifndef _APEX_CPU_H_
+#define _APEX_CPU_H_
+
+#include "apex_macros.h"
+
+/* Format of an APEX instruction  */
+typedef struct APEX_Instruction
+{
+    char opcode_str[128];
+    int opcode;
+    int rd;
+    int rs1;
+    int rs2;
+    int imm;
+} APEX_Instruction;
+
+typedef struct ROB_entry{
+	int pc_value; //instruction address
+	int ar_address; //address of destination architecture register
+	int result;   //result od reg-to-reg or mem address
+	int sval_valid;  //only for STORE
+	int ex_codes;    //exception codes
+	int status;  //indication if the result is valid
+	char inst_type;  //
+}ROB_entry;
+
+
+/* Model of CPU stage latch */
+typedef struct CPU_Stage
+{
+    int pc;
+    char opcode_str[128];
+    int opcode;
+    int rs1;
+    int rs2;
+    int rd;
+    int imm;
+    int rs1_value;
+    int rs2_value;
+    int result_buffer;
+    int memory_address;
+    int has_insn;
+} CPU_Stage;
+
+/* Model of APEX CPU */
+typedef struct APEX_CPU
+{
+    int pc;                        /* Current program counter */
+    int clock;                     /* Clock cycles elapsed */
+    int insn_completed;            /* Instructions retired */
+    int regs[REG_FILE_SIZE];       /* Integer register file */
+    int code_memory_size;          /* Number of instruction in the input file */
+    APEX_Instruction *code_memory; /* Code Memory */
+    int data_memory[DATA_MEMORY_SIZE]; /* Data Memory */
+    int single_step;               /* Wait for user input after every cycle */
+    int zero_flag;                 /* {TRUE, FALSE} Used by BZ and BNZ to branch */
+    int fetch_from_next_cycle;
+
+    /* Pipeline stages */
+    CPU_Stage fetch;
+    CPU_Stage decode;
+    CPU_Stage execute;
+    CPU_Stage memory;
+    CPU_Stage writeback;
+	
+	
+	ROB_entry ROB[ROB_ENTRY_SIZE];
+	int rob_head;
+	int rob_tail;
+} APEX_CPU;
+
+APEX_Instruction *create_code_memory(const char *filename, int *size);
+APEX_CPU *APEX_cpu_init(const char *filename);
+void APEX_cpu_run(APEX_CPU *cpu);
+void APEX_cpu_stop(APEX_CPU *cpu);
+#endif
