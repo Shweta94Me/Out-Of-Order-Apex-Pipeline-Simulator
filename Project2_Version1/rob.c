8dd085c957e1f865cb3dbb9452f93526b427634e
bahaarsadhnani <36011624+bahaarsadhnani@users.noreply.github.com>
2020-12-01 01:22:27 -0500

    Update rob.c

--- Project2_Version1/rob.c
+++ Project2_Version1/rob.c
@@ -14,15 +14,13 @@ void createROB()
     rob->head = NULL;
     rob->tail = NULL;
     rob->size = 0;
-
-    return;
 }
 
 // Queue is full when size becomes
 // equal to the capacity
 int ROB_is_full()
 {
-    return (rob->size >= ROB_ENTRY_SIZE);
+    return (rob->size >= ROB_SIZE);
 }
 
 // Queue is empty when size is 0
@@ -54,7 +52,7 @@ ROB_entry_node *ROB_create_entry(CPU_Stage stage)
         .inst_type = instruction_type(stage.opcode),
         .result = 0,
         .sval_valid = 0,
-        .status = 1,
+        .status = 0,
     };
     node->entry = entry;
     node->next = NULL;
@@ -68,7 +66,6 @@ void ROB_push(CPU_Stage instruction)
     if (ROB_is_full())
         return;
     ROB_entry_node *node = ROB_create_entry(instruction);
-
     if (!rob->tail)
     {
         rob->head = node;
@@ -79,30 +76,24 @@ void ROB_push(CPU_Stage instruction)
     }
     rob->tail = node;
     rob->size++;
-
 }
 
 void ROB_update_RF(ROB_entry entry)
 {
     // TODO:
-    
 }
 
-// Function to remove an item from queue.
-// It changes front and size
 void ROB_pop()
 {
-    if (ROB_is_empty() || !rob->head->entry.status){
-        //printf("reached here");
+    if (ROB_is_empty() || !rob->head->entry.status)
         return;
-    }
-
     ROB_entry_node *node = rob->head;
-    // ROB_update_RF(node->entry);
-    printf("val -> %d \n", node->entry.pc_value);
+    ROB_update_RF(node->entry);
     rob->head = rob->head->next;
     free(node);
     rob->size--;
+    if (!rob->size)
+        rob->head = rob->tail = 0;
 }
 
 void forward_to_rob(CPU_Stage instruction)
@@ -119,89 +110,3 @@ void forward_to_rob(CPU_Stage instruction)
         node = node->next;
     }
 }
-
-// // Unit test code for rob 
-// int main(){
-
-//     createROB();
-//     CPU_Stage cpustage;
-
-//     cpustage.pc = 1;
-//     cpustage.opcode = OPCODE_ADD;
-//     cpustage.rd = 1;
-
-
-//     printf("size -> %d \n", rob->size);  // 0
-//     ROB_push(cpustage);
-//     cpustage.pc = 2;
-//     ROB_push(cpustage);
-//     cpustage.pc = 3;
-//     ROB_push(cpustage);
-//     cpustage.pc = 4;
-//     ROB_push(cpustage);
-//     cpustage.pc = 5;
-//     ROB_push(cpustage);
-//     cpustage.pc = 6;
-//     ROB_push(cpustage);
-//     cpustage.pc = 7;
-//     ROB_push(cpustage);
-//     cpustage.pc = 8;
-//     ROB_push(cpustage);  // this is 8th
-
-//     printf("size -> %d \n", rob->size); // 7
-//     ROB_pop();
-//     ROB_pop();
-//     ROB_pop();
-//     ROB_pop();
-//     ROB_pop();
-//     ROB_pop();
-//     ROB_pop();
-//     ROB_pop();  // this is 8th pop()  
-
-//     printf("size -> %d \n ", rob->size); // 0
-//     cpustage.pc = 9;
-//     ROB_push(cpustage);
-//     ROB_pop();  //this pop cause segmentation fault
-//     ROB_pop();
-//     printf("size -> %d \n", rob->size); // 1
-
-//     return 0;
-// }
-
-
-
-//-------------------------------- Please dont uncomment this below code---------------------------------///
-// if(rob->head == NULL){
-    //     rob->head = node;
-    //     rob->size++;
-    // }else{
-    //     rob->tail->next = node;
-    //     rob->size++;
-    // }
-
-    // rob->tail = node;
-    // rob->tail->next = rob->head;
-
-
-
-    // ROB_entry temp;
-    // if(rob->head == rob->tail){
-    //     temp = rob->head->entry;
-    //     free(rob->head);
-    //     rob->head = NULL;
-    //     rob->tail = NULL;
-    //     rob->size--;
-    // }
-    // else{
-        
-    //     ROB_entry_node* node = rob->head;
-    //     temp = node->entry;
-    //     rob->head = rob->head->next;
-    //     rob->tail->next = rob->head;
-    //     free(node);
-    //     rob->size--;
-    // }
-
-
-
-
